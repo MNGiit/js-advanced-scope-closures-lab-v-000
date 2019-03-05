082d861073dc7fb05ec46997b0859ea7a13946c9
Mohammed Noori <kmanoorib@gmail.com>
2019-03-05 05:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-function produceDrivingRange(number) {
-  return function(str1, str2)
+function produceDrivingRange() {
+  let number = 0;
+  
+  // returns a function that will take two strings as arguments and returns a message
+  return function {
+    r
+  }
 }
 
+function createItem() {
+  let ItemId = 0;
+  // return the class
+  return class {
+    constructor(name, manufacturePrice) {
+      this.name = name;
+      this.manufacturePrice = manufacturePrice;
+      this.id = ++ItemId;
+    }
+ 
+    retailPrice(marketMultiplier) {
+      return marketMultiplier * this.manufacturePrice;
+    }
+  };
+}
+ 
