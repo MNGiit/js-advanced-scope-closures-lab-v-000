19fa6bd6776982b8325bd335758d4f92df1e7747
Mohammed Noori <kmanoorib@gmail.com>
2019-03-05 05:08:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ function produceDrivingRange(range) {
   // returns a function that will take two strings as arguments and returns a message
   return function(stringOne, stringTwo) {
     let stringReturn = "";
+    
     if (range >= 7) {
       stringReturn = "8 blocks out of range";
     } else {
       stringReturn = "within range by 7";
     }
+    
+    return stringReturn;
   };
+  
 }
 
 function createItem() {
